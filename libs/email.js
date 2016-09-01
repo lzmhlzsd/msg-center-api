96f4c9c32d4eda521e2ba5d28da781f253d50226
lzmhlzsd <332847979@qq.com>
2016-09-01 18:26:51 +0800

    m

--- libs/email.js
+++ libs/email.js
@@ -27,43 +27,47 @@ exports.send_notice = function (msg) {
     //        pass: msg.user.c_email_password  //"cyokedaiptaicacf" // 密码
     //    }
     //});
+    try {
+        var transporter = nodemailer.createTransport({
+            host: msg.user.c_email_host, //"smtp.qq.com", // 主机
+            secureConnection: true, // 使用 SSL
+            port: msg.user.c_email_port, //465, // SMTP 端口
+            auth: {
+                user: msg.user.c_email_username, //"332847979@qq.com", // 账号
+                pass: msg.user.c_email_password  //"cyokedaiptaicacf" // 密码
+            }
+        });
 
-    var transporter = nodemailer.createTransport({
-        host: msg.user.c_email_host, //"smtp.qq.com", // 主机
-        secureConnection: true, // 使用 SSL
-        port: msg.user.c_email_port, //465, // SMTP 端口
-        auth: {
-            user: msg.user.c_email_username, //"332847979@qq.com", // 账号
-            pass: msg.user.c_email_password  //"cyokedaiptaicacf" // 密码
-        }
-    });
-
-    u.templateSettings = {
-        interpolate: /\{\{(.+?)\}\}/g
-    };
-    var template = u.template(msg.templatecontent);
+        u.templateSettings = {
+            interpolate: /\{\{(.+?)\}\}/g
+        };
+        var template = u.template(msg.templatecontent);
 
 
-    var emailto = [];
-    u.each(msg.member, function (value, key) {
-        emailto.push(value.c_email);
-    });
+        var emailto = [];
+        u.each(msg.member, function (value, key) {
+            emailto.push(value.c_email);
+        });
 
-    var mailOptions = {
-        from: msg.user.c_email_username, //'332847979@qq.com', // sender address
-        to: emailto.toString(), // list of receivers
-        subject: msg.params.notice_subject, // Subject line
-        text: msg.params.notice_subject + '(' + msg.params.notice_text + ')', // plaintext body
-        html: template(msg.params.notice_data) // html body
-    };
-    console.log(mailOptions)
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            utool.writeNoticeLog(msg, 'email', template(msg.params.notice_data), error);
-            console.log(error);
-        } else {
-            utool.writeNoticeLog(msg, 'email', template(msg.params.notice_data));
-            console.log('邮件发送成功: ' + info.response);
-        }
-    });
+        var mailOptions = {
+            from: msg.user.c_email_username, //'332847979@qq.com', // sender address
+            to: emailto.toString(), // list of receivers
+            subject: msg.params.notice_subject, // Subject line
+            text: msg.params.notice_subject + '(' + msg.params.notice_text + ')', // plaintext body
+            html: template(msg.params.notice_data) // html body
+        };
+        console.log(mailOptions)
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                utool.writeNoticeLog(msg, 'email', template(msg.params.notice_data), error);
+                console.log(error);
+            } else {
+                utool.writeNoticeLog(msg, 'email', template(msg.params.notice_data));
+                console.log('邮件发送成功: ' + info.response);
+            }
+        });
+    }
+    catch(e){
+        utool.writeNoticeLog(msg, 'weixin', '', e.toString());
+    }
 }
